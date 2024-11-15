b0801351379541ba9bba4aa2adcc2edf1362c75f
Andrew <alpries@comcast.net>
2024-11-15 15:56:54 -0500

    added the script

---
+++ start_servers.sh
@@ -0,0 +1,5 @@
+gnome-terminal -- bash -c "node server.js; exec bash"
+
+gnome-terminal -- bash -c "./main; exec bash"
+
+gnome-terminal -- bash -c "ngrok start my-http-tunnel my-tcp-tunnel; exec bash"
